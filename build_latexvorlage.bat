27895b97e43864d260a5470f8f7760c0c927cf34
OvidiusCicero <OvidiusCicero@users.noreply.github.com>
2018-05-21 21:13:17 +0200

    Schriftart zwischen

--- build_latexvorlage.bat
+++ build_latexvorlage.bat
@@ -1,4 +1,4 @@
-call build_base.bat --to=latex-smart --pdf-engine=xelatex --template=vorlagen/latex.tex --output=latex_out/gespraech_%2-%3.tex --variable=geometry:"a4paper, margin=2.5cm, %2" --variable=romanfont:"Linux Libertine" --variable=sansfont:"Linux Biolinum" --variable=numcols:"%1" --variable=%3 --variable=%2 --variable=graphics --variable=documentclass:scrartcl
+call build_base.bat --to=latex-smart --pdf-engine=xelatex --template=vorlagen/latex.tex --output=latex_out/gespraech_%2-%3.tex --variable=geometry:"a4paper, margin=2.5cm, %2" --variable=romanfont:"Linux Libertine O" --variable=sansfont:"Linux Biolinum O" --variable=numcols:"%1" --variable=%3 --variable=%2 --variable=graphics --variable=documentclass:scrartcl
 cd latex_out
 xelatex --interaction=nonstopmode gespraech_%2-%3.tex
 cd ..
