6533fb59f1be45632005f618137e6c302a3422b1
OvidiusCicero <OvidiusCicero@users.noreply.github.com>
2018-06-02 11:36:09 +0200

    Reste

--- build_htmlfonts.bat
+++ build_htmlfonts.bat
@@ -1,1 +1,1 @@
-build_base.bat --to=html5 --self-contained --template=vorlagen/html.xhtml --output=out/gespraech.xhtml --css=fonts.css --css=style.css
+build_base.bat --to=html5 --self-contained --template=vorlagen/html.xhtml --output=out/gespraech_fonts.xhtml --css=fonts.css --css=style.css
